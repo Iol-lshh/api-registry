941fb5969645258ff5d2681f4feb1bbe25495cc8
luke0409 <luke0409@chunjae.co.kr>
2024-04-01 13:01:33 +0900

    port 도입

---
+++ skeleton/src/main/java/org/lshh/skeleton/core/port/ApiRegistryPort.java
@@ -0,0 +1,8 @@
+package org.lshh.skeleton.core.port;
+
+public class ApiRegistryPort {
+    // 요청받음 -> 오케스트레이터 생성 -> 오케스트레이터 처리 -> 응답반환
+    // 비동기? 동기?
+        // 처리 방식(동기? 비동기?), 반환 방식(동기? 비동기?)
+    // lazy 모드(후 콜백 반환)와 eager(바로 반환) 모드
+}
---
+++ skeleton/src/main/java/org/lshh/skeleton/core/port/Orchestrator.java
@@ -0,0 +1,6 @@
+package org.lshh.skeleton.core.port;
+
+public class Orchestrator {
+    // 이벤트
+
+}
