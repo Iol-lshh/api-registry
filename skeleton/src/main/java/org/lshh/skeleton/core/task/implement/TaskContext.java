1ad606d3d5f5778a27de90867b0355cec1012425
luke0409 <luke0409@chunjae.co.kr>
2024-04-02 14:17:09 +0900

    테스크 컨텍스트 엔티티 수정

--- skeleton/src/main/java/org/lshh/skeleton/core/task/implement/TaskContext.java
+++ skeleton/src/main/java/org/lshh/skeleton/core/task/implement/TaskContext.java
@@ -1,8 +1,12 @@
 package org.lshh.skeleton.core.task.implement;
 
+import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import org.lshh.skeleton.core.task.Task.TaskType;
+
+import java.util.List;
 
 @Getter
 @Entity
@@ -12,5 +16,32 @@ public class TaskContext {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
+    @NotNull
     TaskType type;
+
+    Long queryId;
+    Long resourceId;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_task_id")
+    TaskContext parentTask;
+
+    @OneToMany(mappedBy = "parentTask")
+    List<TaskContext> subTasks;
+
+    public static TaskContext of(TaskType type, @Nullable Long queryId, @Nullable Long resourceId) {
+        TaskContext context = new TaskContext();
+        context.type = type;
+        context.queryId = queryId;
+        context.resourceId = resourceId;
+        return context;
+    }
+
+    public static TaskContext of(TaskType type, @Nullable TaskContext parentTask, @Nullable List<TaskContext> subTasks) {
+        TaskContext context = new TaskContext();
+        context.type = type;
+        context.parentTask = parentTask;
+        context.subTasks = subTasks;
+        return context;
+    }
 }
