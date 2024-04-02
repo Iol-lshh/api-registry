62a627c19504425a15906cd5d06557429326dac9
luke0409 <luke0409@chunjae.co.kr>
2024-04-02 12:50:04 +0900

    커넥션 풀 추가

--- skeleton/src/main/java/org/lshh/skeleton/core/resource/resourcer/JdbcResourcer.java
+++ skeleton/src/main/java/org/lshh/skeleton/core/resource/resourcer/JdbcResourcer.java
@@ -3,10 +3,14 @@ package org.lshh.skeleton.core.resource.resourcer;
 import javax.sql.DataSource;
 
 public interface JdbcResourcer extends Resourcer{
-
     @Override
     default ResourcerType getType(){
         return ResourcerType.RDBMS;
     }
     DataSource getDataSource();
+
+    void initDataSource();
+
+    // 커넥션 풀 관리 필요?
+    JdbcResourcer setConectionPool(int maxPoolSize, int minIdle, long connectionTimeout, long idleTimeout, long maxLifetime);
 }
--- skeleton/src/main/java/org/lshh/skeleton/core/resource/resourcer/implement/SimpleJdbcResourcer.java
+++ skeleton/src/main/java/org/lshh/skeleton/core/resource/resourcer/implement/SimpleJdbcResourcer.java
@@ -18,18 +18,35 @@ public class SimpleJdbcResourcer implements JdbcResourcer {
         return new SimpleJdbcResourcer(context);
     }
 
+    @Override
     public void initDataSource() {
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(context.getUrl());
         config.setUsername(context.getUsername());
         config.setPassword(context.getPassword());
         config.setDriverClassName(context.getAdaptorName());
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(5);
+        config.setConnectionTimeout(30000);
+        config.setIdleTimeout(600000);
+        config.setMaxLifetime(1800000);
         this.dataSource = new HikariDataSource(config);
     }
 
     @Override
     public DataSource getDataSource() {
         return this.dataSource;
+    }
+
+    @Override
+    public JdbcResourcer setConectionPool(int maxPoolSize, int minIdle, long connectionTimeout, long idleTimeout, long maxLifetime) {
+        HikariDataSource hikariDataSource = (HikariDataSource) this.dataSource;
+        hikariDataSource.setMaximumPoolSize(maxPoolSize);
+        hikariDataSource.setMinimumIdle(minIdle);
+        hikariDataSource.setConnectionTimeout(connectionTimeout);
+        hikariDataSource.setIdleTimeout(idleTimeout);
+        hikariDataSource.setMaxLifetime(maxLifetime);
+        return this;
     }
 
     @Override
