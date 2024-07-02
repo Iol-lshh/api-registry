862169ea44bd808b9afe36c184028b8b6ae3727c
lshh <markhong93@naver.com>
2024-07-02 18:16:07 +0900

    task 삭제

--- skeleton/src/main/java/org/lshh/skeleton/core/task/exception/TaskContextException.java
+++
@@ -1,7 +0,0 @@
-package org.lshh.skeleton.core.task.exception;
-
-public class TaskContextException extends RuntimeException{
-    public TaskContextException(String message) {
-        super(message);
-    }
-}
